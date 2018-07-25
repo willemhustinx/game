e431fba4afb846ac81c4daf59e7c027363a9d854
Willem Hustinx <willemhustinx@gmail.com>
2018-07-25 15:35:31 +0200

    first Game camera
    https://www.codingmadesimple.com/module-1/unit-6-game-camera/

--- src/nl/willemhustinx/game/Game.java
+++ src/nl/willemhustinx/game/Game.java
@@ -11,6 +11,7 @@ public class Game extends Canvas implements Runnable {
     private boolean isRunning = false;
     private Thread thread;
     private Handler handler;
+    private Camera camera;
 
     private BufferedImage level = null;
 
@@ -19,6 +20,7 @@ public class Game extends Canvas implements Runnable {
         start();
 
         handler = new Handler();
+        camera = new Camera(0, 0);
         this.addKeyListener(new KeyInput(handler));
 
         BufferdImageLoader bufferdImageLoader = new BufferdImageLoader();
@@ -75,6 +77,12 @@ public class Game extends Canvas implements Runnable {
     }
 
     public void tick() {
+        for (int i = 0; i < handler.object.size(); i++) {
+            if (handler.object.get(i).getId() == ID.Player) {
+                camera.tick(handler.object.get(i));
+            }
+        }
+
         handler.tick();
     }
 
@@ -86,11 +94,17 @@ public class Game extends Canvas implements Runnable {
         }
 
         Graphics g = bs.getDrawGraphics();
+        Graphics2D g2d = (Graphics2D) g;
+
 
         g.setColor(Color.red);
         g.fillRect(0, 0, 1000, 563);
 
+        g2d.translate(-camera.getX(), -camera.getY());
+
         handler.render(g);
+
+        g2d.translate(camera.getX(), camera.getY());
 
         g.dispose();
         bs.show();
